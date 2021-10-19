57d039f96b1560bdccef0b2f612337c385ebcd68
Filippos Christianos <f.christianos@ed.ac.uk>
2021-10-19 14:43:58 +0100

    Feature: registers environment with gym

--- bpush/__init__.py
+++ bpush/__init__.py
@@ -1,11 +1,26 @@
 import gym
-
+from gym.envs.registration import register
 # from bpush import BoulderPush, RewardType, Action
 import itertools
 
+
 _sizes = {
-    "tiny": (1, 3),
-    "small": (2, 3),
-    "medium": (2, 5),
-    "large": (3, 5),
+    "tiny": (5, 5),
+    "small": (8, 8),
+    "medium": (12, 12),
+    "large": (20, 20),
 }
+
+for size in _sizes.keys():
+    for n_agents in range(1, 5):
+
+        register(
+        id=f"bpush-{size}-{n_agents}ag-v0",
+        entry_point="bpush.environment:BoulderPush",
+        kwargs={
+            "height": _sizes[size][0],
+            "width": _sizes[size][1],
+            "n_agents": n_agents,
+            "sensor_range": 4,
+        },
+)
