d5ecb343ae4370de9683d63b7a4cf94b51a48d4e
Filippos Christianos <fchristianos@ed.ac.uk>
2022-05-19 11:49:39 +0100

    Feature: difficulty levels for increasing penalty

--- bpush/__init__.py
+++ bpush/__init__.py
@@ -11,16 +11,24 @@ _sizes = {
     "large": (20, 20),
 }
 
+_penalties = {
+    "easy": 0.01,
+    "medium": 0.05,
+    "hard": 0.1,
+    "vhard": 0.2,
+}
+
 for size in _sizes.keys():
     for n_agents in range(1, 5):
-
-        register(
-        id=f"bpush-{size}-{n_agents}ag-v0",
-        entry_point="bpush.environment:BoulderPush",
-        kwargs={
-            "height": _sizes[size][0],
-            "width": _sizes[size][1],
-            "n_agents": n_agents,
-            "sensor_range": 4,
-        },
+        for penalty in _penalties.keys():
+            register(
+            id=f"bpush-{size}-{n_agents}ag-{penalty}-v0",
+            entry_point="bpush.environment:BoulderPush",
+            kwargs={
+                "height": _sizes[size][0],
+                "width": _sizes[size][1],
+                "n_agents": n_agents,
+                "sensor_range": 4,
+                "penalty": _penalties[penalty]
+            },
 )
